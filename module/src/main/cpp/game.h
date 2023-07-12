8bbd83784cfa7afdd04a64cdb3677a48f25fd9ac
Hai2007 <134753588+Hai2007@users.noreply.github.com>
2023-07-13 06:15:11 +0700

    Update game.h

--- module/src/main/cpp/game.h
+++ module/src/main/cpp/game.h
@@ -5,6 +5,6 @@
 #ifndef ZYGISK_IL2CPPDUMPER_GAME_H
 #define ZYGISK_IL2CPPDUMPER_GAME_H
 
-#define GamePackageName "com.loadcomplete.slimeidle"
+#define GamePackageName "com.vng.pubgmobile"
 
 #endif //ZYGISK_IL2CPPDUMPER_GAME_H
