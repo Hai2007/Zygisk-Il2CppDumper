c0147e19fb2f8824f66f46220d4d2b977f217b92
Hai2007 <134753588+Hai2007@users.noreply.github.com>
2023-07-12 14:40:13 +0700

    Update game.h

--- module/src/main/cpp/game.h
+++ module/src/main/cpp/game.h
@@ -5,6 +5,6 @@
 #ifndef ZYGISK_IL2CPPDUMPER_GAME_H
 #define ZYGISK_IL2CPPDUMPER_GAME_H
 
-#define GamePackageName "com.gear2.growslayer"
+#define GamePackageName "com.vng.mobile"
 
 #endif //ZYGISK_IL2CPPDUMPER_GAME_H
