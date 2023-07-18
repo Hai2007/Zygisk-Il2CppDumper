88c2691c5256096651bdb1b4bb83fa9c88a8e9fd
Hai2007 <134753588+Hai2007@users.noreply.github.com>
2023-07-18 13:19:26 +0700

    Update game.h

--- module/src/main/cpp/game.h
+++ module/src/main/cpp/game.h
@@ -5,6 +5,6 @@
 #ifndef ZYGISK_IL2CPPDUMPER_GAME_H
 #define ZYGISK_IL2CPPDUMPER_GAME_H
 
-#define GamePackageName "com.cookapps.newdemonslayer"
+#define GamePackageName "com.idle.pseudo3d.hero"
 
 #endif //ZYGISK_IL2CPPDUMPER_GAME_H
