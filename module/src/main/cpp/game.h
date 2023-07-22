188f9fe145a4cc80e840a429699aa00cf5a582c1
Hai2007 <134753588+Hai2007@users.noreply.github.com>
2023-07-22 20:02:26 +0700

    Update game.h

--- module/src/main/cpp/game.h
+++ module/src/main/cpp/game.h
@@ -5,6 +5,6 @@
 #ifndef ZYGISK_IL2CPPDUMPER_GAME_H
 #define ZYGISK_IL2CPPDUMPER_GAME_H
 
-#define GamePackageName "com.SyNguyen.ChibiSurvivorOnlineSinhtncngbnb"
+#define GamePackageName "com.klsvt.az"
 
 #endif //ZYGISK_IL2CPPDUMPER_GAME_H
