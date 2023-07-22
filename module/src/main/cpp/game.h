ff1189e03a3a7031a1af17e72599ef217fc65ed3
Hai2007 <134753588+Hai2007@users.noreply.github.com>
2023-07-22 20:06:54 +0700

    Update game.h

--- module/src/main/cpp/game.h
+++ module/src/main/cpp/game.h
@@ -5,6 +5,6 @@
 #ifndef ZYGISK_IL2CPPDUMPER_GAME_H
 #define ZYGISK_IL2CPPDUMPER_GAME_H
 
-#define GamePackageName "com.klsvt.az"
+#define GamePackageName "com.camelgames.aoz.vn"
 
 #endif //ZYGISK_IL2CPPDUMPER_GAME_H
