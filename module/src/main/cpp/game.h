aafd48bd1e71a4a642ed3007774490b71891827c
Hai2007 <134753588+Hai2007@users.noreply.github.com>
2023-07-29 18:32:00 +0700

    Update game.h

--- module/src/main/cpp/game.h
+++ module/src/main/cpp/game.h
@@ -5,6 +5,6 @@
 #ifndef ZYGISK_IL2CPPDUMPER_GAME_H
 #define ZYGISK_IL2CPPDUMPER_GAME_H
 
-#define GamePackageName "com.mechanist.poi.vn"
+#define GamePackageName "com.mobirix.g2gmg"
 
 #endif //ZYGISK_IL2CPPDUMPER_GAME_H
