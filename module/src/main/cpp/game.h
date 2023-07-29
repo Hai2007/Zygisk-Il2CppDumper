1afebb1ac0fd35e13cc22254949f73d0c1bae14d
Hai2007 <134753588+Hai2007@users.noreply.github.com>
2023-07-29 22:43:16 +0700

    Update game.h

--- module/src/main/cpp/game.h
+++ module/src/main/cpp/game.h
@@ -5,6 +5,6 @@
 #ifndef ZYGISK_IL2CPPDUMPER_GAME_H
 #define ZYGISK_IL2CPPDUMPER_GAME_H
 
-#define GamePackageName "com.mobirix.g2gmg"
+#define GamePackageName "com.combo.heroes.puzzle.rpg"
 
 #endif //ZYGISK_IL2CPPDUMPER_GAME_H
