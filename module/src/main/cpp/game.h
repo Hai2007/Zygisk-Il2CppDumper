51646d66467fde155db7d87189b38b5c502f92e0
Hai2007 <134753588+Hai2007@users.noreply.github.com>
2023-07-30 13:15:13 +0700

    Update game.h

--- module/src/main/cpp/game.h
+++ module/src/main/cpp/game.h
@@ -5,6 +5,6 @@
 #ifndef ZYGISK_IL2CPPDUMPER_GAME_H
 #define ZYGISK_IL2CPPDUMPER_GAME_H
 
-#define GamePackageName "com.gameloft.android.ANMP.GloftDOHM"
+#define GamePackageName "com.cyg.novafly.us.gp"
 
 #endif //ZYGISK_IL2CPPDUMPER_GAME_H
