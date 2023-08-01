db768c23658f0d31cdfa412ff8ebe2bf418a6693
Hai2007 <134753588+Hai2007@users.noreply.github.com>
2023-08-01 17:12:44 +0700

    Update game.h

--- module/src/main/cpp/game.h
+++ module/src/main/cpp/game.h
@@ -5,6 +5,6 @@
 #ifndef ZYGISK_IL2CPPDUMPER_GAME_H
 #define ZYGISK_IL2CPPDUMPER_GAME_H
 
-#define GamePackageName "com.superplanet.swordmaster"
+#define GamePackageName "com.studiodrill.dragonmaster"
 
 #endif //ZYGISK_IL2CPPDUMPER_GAME_H
