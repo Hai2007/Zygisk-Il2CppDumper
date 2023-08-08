5cae9184c720abdd5527f30885a084dd01981348
Hai2007 <134753588+Hai2007@users.noreply.github.com>
2023-08-08 11:49:28 +0700

    Update game.h

--- module/src/main/cpp/game.h
+++ module/src/main/cpp/game.h
@@ -5,6 +5,6 @@
 #ifndef ZYGISK_IL2CPPDUMPER_GAME_H
 #define ZYGISK_IL2CPPDUMPER_GAME_H
 
-#define GamePackageName "com.rmage.official"
+#define GamePackageName "com.com2usholdings.fs.android.google.global.normal"
 
 #endif //ZYGISK_IL2CPPDUMPER_GAME_H
