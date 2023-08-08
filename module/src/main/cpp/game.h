e857b6e8d66d75f0b76b8c3ef4547265f93c6902
Hai2007 <134753588+Hai2007@users.noreply.github.com>
2023-08-08 20:02:40 +0700

    Update game.h

--- module/src/main/cpp/game.h
+++ module/src/main/cpp/game.h
@@ -5,6 +5,6 @@
 #ifndef ZYGISK_IL2CPPDUMPER_GAME_H
 #define ZYGISK_IL2CPPDUMPER_GAME_H
 
-#define GamePackageName "com.com2usholdings.fs.android.google.global.normal"
+#define GamePackageName "com.nostellar.ghostslayer"
 
 #endif //ZYGISK_IL2CPPDUMPER_GAME_H
