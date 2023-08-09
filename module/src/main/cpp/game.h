39f4b7f14ab5920021acff26bd4eb885d406eaa5
Hai2007 <134753588+Hai2007@users.noreply.github.com>
2023-08-09 11:14:24 +0700

    Update game.h

--- module/src/main/cpp/game.h
+++ module/src/main/cpp/game.h
@@ -5,6 +5,6 @@
 #ifndef ZYGISK_IL2CPPDUMPER_GAME_H
 #define ZYGISK_IL2CPPDUMPER_GAME_H
 
-#define GamePackageName "com.percent.royaldice"
+#define GamePackageName "com.AbleGames.DalToKi"
 
 #endif //ZYGISK_IL2CPPDUMPER_GAME_H
