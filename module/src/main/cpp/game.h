f59823aa0dc5d7140e1fbb6613e765cad537758a
Hai2007 <134753588+Hai2007@users.noreply.github.com>
2023-08-11 19:21:47 +0700

    Update game.h

--- module/src/main/cpp/game.h
+++ module/src/main/cpp/game.h
@@ -5,6 +5,6 @@
 #ifndef ZYGISK_IL2CPPDUMPER_GAME_H
 #define ZYGISK_IL2CPPDUMPER_GAME_H
 
-#define GamePackageName "com.awesomepiece.castle"
+#define GamePackageName "com.hunt.royale"
 
 #endif //ZYGISK_IL2CPPDUMPER_GAME_H
