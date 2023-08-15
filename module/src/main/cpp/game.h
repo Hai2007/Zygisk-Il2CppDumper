c934b586083840f33ef4cf6f8a582b22284a5e26
Hai2007 <134753588+Hai2007@users.noreply.github.com>
2023-08-15 17:25:23 +0700

    Update game.h

--- module/src/main/cpp/game.h
+++ module/src/main/cpp/game.h
@@ -5,6 +5,6 @@
 #ifndef ZYGISK_IL2CPPDUMPER_GAME_H
 #define ZYGISK_IL2CPPDUMPER_GAME_H
 
-#define GamePackageName "com.xinshengfun.ldlm.en"
+#define GamePackageName "com.habby.archero"
 
 #endif //ZYGISK_IL2CPPDUMPER_GAME_H
