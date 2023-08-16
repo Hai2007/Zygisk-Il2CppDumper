ee6860ad987ccf697bb74eb50deb78af471f1fd5
Hai2007 <134753588+Hai2007@users.noreply.github.com>
2023-08-16 17:52:54 +0700

    Update game.h

--- module/src/main/cpp/game.h
+++ module/src/main/cpp/game.h
@@ -5,6 +5,6 @@
 #ifndef ZYGISK_IL2CPPDUMPER_GAME_H
 #define ZYGISK_IL2CPPDUMPER_GAME_H
 
-#define GamePackageName "com.habby.archero"
+#define GamePackageName "com.netmarble.skiagb"
 
 #endif //ZYGISK_IL2CPPDUMPER_GAME_H
