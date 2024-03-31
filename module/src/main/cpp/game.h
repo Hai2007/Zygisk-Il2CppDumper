a814bd4f074a1a10409be2d1e3f549039c271881
Hai2007 <134753588+Hai2007@users.noreply.github.com>
2024-03-31 10:20:15 +0700

    Update game.h

--- module/src/main/cpp/game.h
+++ module/src/main/cpp/game.h
@@ -5,6 +5,6 @@
 #ifndef ZYGISK_IL2CPPDUMPER_GAME_H
 #define ZYGISK_IL2CPPDUMPER_GAME_H
 
-#define GamePackageName "com.global.dqvn"
+#define GamePackageName "com.brotato.shooting.survivors.action.roguelike"
 
 #endif //ZYGISK_IL2CPPDUMPER_GAME_H
